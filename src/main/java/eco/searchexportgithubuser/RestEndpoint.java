e2046ccdb345f7a4826c0946173c546a44d2ed68
Eko P <abumkha@gmail.com>
2023-05-23 09:35:09 +0700

    Get github user

--- src/main/java/eco/searchexportgithubuser/RestEndpoint.java
+++ src/main/java/eco/searchexportgithubuser/RestEndpoint.java
@@ -1,34 +1,26 @@
 package eco.searchexportgithubuser;
 
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.util.List;
-
 
 @RestController
 public class RestEndpoint {
 
     @GetMapping("/{token}")
-    public List<GithubUser> homePage(@PathVariable String token) {
+    public ResponseEntity<String> homePage(@PathVariable String token) {
 
         RestTemplate restTemplate = new RestTemplate();
-        URI uri=URI.create("https://api.github.com/search/users?q=Q");
-        ResponseEntity<List<GithubUser>> response = restTemplate.exchange(
-                uri, HttpMethod.GET, null,
-                new ParameterizedTypeReference<>() {
-                });
-
-        List<GithubUser> result = response.getBody();
+        String resourceUrl
+                = "https://api.github.com/search/users?q=Q";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(resourceUrl, String.class);
 
 //        return "hello "+token;
-        return result;
+        return response;
     }
 
 }
