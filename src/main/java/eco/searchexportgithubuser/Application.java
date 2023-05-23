66d863d17603c84d8cd0cb277e11610085b4fc61
Eko P <abumkha@gmail.com>
2023-05-23 08:40:39 +0700

    Refactor to class PdfTryer

--- src/main/java/eco/searchexportgithubuser/Application.java
+++ src/main/java/eco/searchexportgithubuser/Application.java
@@ -1,32 +1,19 @@
 package eco.searchexportgithubuser;
 
-import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.DocumentException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws DocumentException, FileNotFoundException {
+    public static void main(String[] args) throws DocumentException, IOException {
 
-        tryPdf();
+        new PdfTryer().tryPdf();
 
 //        SpringApplication.run(Application.class, args);
     }
 
-    private static void tryPdf() throws DocumentException, FileNotFoundException {
-        Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
-
-        document.open();
-        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-        Chunk chunk = new Chunk("Hello World", font);
-
-        document.add(chunk);
-        document.close();
-    }
 
 }
