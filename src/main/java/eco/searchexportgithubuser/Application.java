90cae2def4643b90a3f8495ccc3cfd9acb7e3e56
Eko P <abumkha@gmail.com>
2023-05-23 07:16:45 +0700

    Try PDF

--- src/main/java/eco/searchexportgithubuser/Application.java
+++ src/main/java/eco/searchexportgithubuser/Application.java
@@ -1,12 +1,32 @@
 package eco.searchexportgithubuser;
 
-import org.springframework.boot.SpringApplication;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws DocumentException, FileNotFoundException {
+
+        tryPdf();
+
+//        SpringApplication.run(Application.class, args);
     }
+
+    private static void tryPdf() throws DocumentException, FileNotFoundException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Chunk chunk = new Chunk("Hello World", font);
+
+        document.add(chunk);
+        document.close();
+    }
+
 }
