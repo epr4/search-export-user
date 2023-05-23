515c605a4f9abcf1b720b3e336105c9d898310e1
Eko P <abumkha@gmail.com>
2023-05-23 09:45:19 +0700

    Add GithubUserPdfWriter class

---
+++ src/main/java/eco/searchexportgithubuser/GithubUserPdfWriter.java
@@ -0,0 +1,87 @@
+package eco.searchexportgithubuser;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+
+public class GithubUserPdfWriter {
+
+    String raw;
+    public GithubUserPdfWriter(String raw) {
+        this.raw=raw;
+    }
+
+    public void writeToPdf() throws DocumentException, IOException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("iTextTable.pdf"));
+
+        document.open();
+
+        PdfPTable table = new PdfPTable(3);
+        addTableHeader(table);
+
+        addRows(table);
+        int row=2;
+        for (;row<=64;row++) {
+            addRow(table,row);
+        }
+
+        addCustomRows(table);
+
+        document.add(table);
+        document.close();
+    }
+
+    private void addRow(PdfPTable table, int row) {
+        table.addCell("row "+row+", col 1");
+        table.addCell("row "+row+", col 2");
+        table.addCell("row "+row+", col 3");
+    }
+
+    private void addTableHeader(PdfPTable table) {
+        Stream.of("column header 1", "column header 2", "column header 3")
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columnTitle));
+                    table.addCell(header);
+                });
+    }
+
+    private void addRows(PdfPTable table) {
+        table.addCell("row 1, col 1");
+        table.addCell("row 1, col 2");
+        table.addCell("row 1, col 3");
+    }
+
+    private void addCustomRows(PdfPTable table)
+//            throws URISyntaxException, BadElementException, IOException
+    {
+/*
+        Path path = Paths.get(ClassLoader.getSystemResource("Java_logo.png").toURI());
+        Image img = Image.getInstance(path.toAbsolutePath().toString());
+        img.scalePercent(10);
+
+        PdfPCell imageCell = new PdfPCell(img);
+        table.addCell(imageCell);
+*/
+
+        PdfPCell horizontalAlignCell = new PdfPCell(new Phrase("row 2, col 2"));
+        horizontalAlignCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(horizontalAlignCell);
+
+        PdfPCell verticalAlignCell = new PdfPCell(new Phrase("row 2, col 3"));
+        verticalAlignCell.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        table.addCell(verticalAlignCell);
+
+
+    }
+
+}
--- src/main/java/eco/searchexportgithubuser/RestEndpoint.java
+++ src/main/java/eco/searchexportgithubuser/RestEndpoint.java
@@ -1,23 +1,29 @@
 package eco.searchexportgithubuser;
 
+import com.itextpdf.text.DocumentException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+
 
 @RestController
 public class RestEndpoint {
 
     @GetMapping("/{token}")
-    public ResponseEntity<String> homePage(@PathVariable String token) {
+    public ResponseEntity<String> homePage(@PathVariable String token) throws DocumentException, IOException {
 
         RestTemplate restTemplate = new RestTemplate();
         String resourceUrl
                 = "https://api.github.com/search/users?q=Q";
         ResponseEntity<String> response
                 = restTemplate.getForEntity(resourceUrl, String.class);
+
+        GithubUserPdfWriter githubUserPdfWriter = new GithubUserPdfWriter(response.getBody());
+        githubUserPdfWriter.writeToPdf();
 
 //        return "hello "+token;
         return response;
