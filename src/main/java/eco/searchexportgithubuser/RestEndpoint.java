ad0aa70091c5784b4c3d2519cf1e7dccb1ef8751
Eko P <abumkha@gmail.com>
2023-05-24 09:06:25 +0700

    Download File

--- src/main/java/eco/searchexportgithubuser/RestEndpoint.java
+++ src/main/java/eco/searchexportgithubuser/RestEndpoint.java
@@ -3,14 +3,22 @@ package eco.searchexportgithubuser;
 import com.itextpdf.text.DocumentException;
 import eco.searchexportgithubuser.db.History;
 import eco.searchexportgithubuser.db.HistoryDao;
+import org.apache.pdfbox.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 
@@ -43,6 +51,52 @@ public class RestEndpoint {
     @GetMapping("/history")
     public List<History> listHistory() {
         return historyDao.findAll();
+    }
+
+    @GetMapping(value = "/donwload", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody byte[] donwload() throws IOException {
+/*
+        InputStream in = getClass()
+//                .getResourceAsStream("/com/baeldung/produceimage/image.jpg");
+                .getResourceAsStream("iTextTable.pdf");
+*/
+        String fileName = "iTextTable.pdf";
+        InputStream in = new FileInputStream(fileName);
+        return IOUtils.toByteArray(in);
+
+/*
+        String contentType = "application/octet-stream";
+        String headerValue = "attachment; filename=\"" + fileName + "\"";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+                .body(resource);
+*/
+    }
+
+    @GetMapping("/downloadFile/{fileCode}")
+    public ResponseEntity<?> downloadFile(@PathVariable("fileCode") String fileCode) {
+        FileDownloadUtil downloadUtil = new FileDownloadUtil();
+
+        Resource resource = null;
+        try {
+            resource = downloadUtil.getFileAsResource(fileCode);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        if (resource == null) {
+            return new ResponseEntity<>("File not found", HttpStatus.NOT_FOUND);
+        }
+
+        String contentType = "application/octet-stream";
+        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+                .body(resource);
     }
 
 }
